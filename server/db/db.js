712c5ec5acc30084db9f5ec48bc1bf80079365da
Oluwaleye VIctor <oluwasquarelsv@gmail.com>
2021-10-07 20:57:41 +0100

    setup local database

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+    logging: false,
+    host: "localhost",
+    username: "root",
+    password: "root",
 });
 
 module.exports = db;
