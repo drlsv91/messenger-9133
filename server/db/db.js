82d20baf3a7f0493fee1c8ce511fe50949ab3364
Oluwaleye VIctor <oluwasquarelsv@gmail.com>
2021-10-09 17:01:55 +0100

    add database crendentials to env file

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,9 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5433/messenger", {
     logging: false,
-    host: "localhost",
-    username: "postgres",
-    password: "root",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
 });
 
 module.exports = db;
